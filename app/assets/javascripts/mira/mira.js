f6ee8dc6eebb035bc269bad35e13ba3cd92906e3
Heirian <shinkcc@gmail.com>
2018-01-06 16:57:09 -0200

    Add a mira.js file to hold all javascript files calls of mira basic template

---
+++ app/assets/javascripts/mira/mira.js
@@ -0,0 +1,10 @@
+//= require mira/bootstrap.min
+//= require mira/jquery.meanmenu
+//= require mira/isotope.pkgd.min
+//= require mira/imagesloaded.pkgd.min
+//= require mira/owl.carousel.min
+//= require mira/jquery.counterup.min
+//= require mira/waypoints.min
+//= require mira/ajax-mail
+//= require mira/plugins
+//= require mira/main
