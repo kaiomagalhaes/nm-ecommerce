00f0a7d247d01403763c04a74c627dec0f7251f8
Heirian <shinkcc@gmail.com>
2018-01-07 17:59:25 -0200

    Remove unused css class addition in taxons tree builder method

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -12,8 +12,7 @@ module Spree
     def taxons_tree_builder(root_taxon, current_taxon, max_level)
       content_tag :ul, class: 'dropdown-menu taxons-list' do
         taxons = root_taxon.children.map do |taxon|
-          css_class = current_taxon&.self_and_ancestors&.include?(taxon) ? 'active' : nil
-          content_tag :li, class: css_class do
+          content_tag :li do
             link_to(taxon.name, seo_url(taxon)) +
               taxons_tree(taxon, current_taxon, max_level - 1)
           end
