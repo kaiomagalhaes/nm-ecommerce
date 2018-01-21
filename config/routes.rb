61f3df4417c14a18a09285c67b437385bb1029b7
Heirian <shinkcc@gmail.com>
2018-01-21 16:39:53 -0200

    Adds about route

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   Spree::Core::Engine.routes.draw do
     get '/contact', to: 'contact#index'
+    get '/about', to: 'about#index'
   end
 
   # Serve websocket cable requests in-process
